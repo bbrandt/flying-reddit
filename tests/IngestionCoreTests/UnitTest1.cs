cfb147064d93d68a1d316d350aa2f39dfc2d6e53
Ben Brandt <bbrandt@users.noreply.github.com>
2023-09-04 16:40:21 -0500

    Add test project

---
+++ tests/IngestionCoreTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace IngestionCoreTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
