73e4b4b02ab8eba28be846244fd1991b87533e7a
Ben Brandt <bbrandt@users.noreply.github.com>
2023-09-05 12:57:45 -0500

    Add placeholder for module DI

---
+++ src/UpstashInfrastructure/Configuration/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZiggyCreatures.Caching.Fusion;
+
+namespace UpstashInfrastructure.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCaching(this IServiceCollection services)
+    {
+        services.AddFusionCache()
+            .WithDefaultEntryOptions(new FusionCacheEntryOptions
+            {
+                Duration = TimeSpan.FromMinutes(2)
+            });
+        return services;
+    }
+
+    public static IServiceCollection AddDatastore(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
